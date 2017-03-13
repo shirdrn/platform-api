a68312eddcc277276bd029899890760ee1885814
yanjun <yanjun.shi@tendcloud.com>
2017-03-13 10:02:05 +0800

    Add an utility method 'startAll' to start all components.

--- src/main/java/cn/shiyanjun/platform/api/utils/ComponentUtils.java
+++ src/main/java/cn/shiyanjun/platform/api/utils/ComponentUtils.java
@@ -4,6 +4,12 @@ import cn.shiyanjun.platform.api.LifecycleAware;
 
 public class ComponentUtils {
 
+	public static void startAll(LifecycleAware... components) {
+		for(LifecycleAware c : components) {
+			c.start();
+		}
+	}
+	
 	public static void stopAllQuietly(LifecycleAware... components) {
 		if(components != null) {
 			for(LifecycleAware c : components) {
